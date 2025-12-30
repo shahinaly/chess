b83e919f9ec114ae8e32f05e1e66bf245ed57dd7
Aly Shahin <shahia60@mcmaster.ca>
2025-12-30 02:22:31 -0500

    Update move number and active color when move is pushed

--- src/game.py
+++ src/game.py
@@ -18,28 +18,38 @@ class Game:
       self.full_move = int(self.full_move)
 
    def __str__(self):
-      print("—"*64)
+      print("—"*72)
       print(self.board, end = '')
-      print("—"*64)
+      print("—"*72)
       print(
-         "Active color:    " + self.active_color,
-         "Move number:     " + str(self.full_move),
-         "Castling:        " + self.castling,
-         "En passant:      " + self.en_passant,
-         "FEN:             " + self.board.fen,
+         "Active color:             " + self.active_color,
+         "Current Move number:      " + str(self.full_move),
+         "Castling:                 " + self.castling,
+         "En passant:               " + self.en_passant,
+         "FEN:                      " + self.board.fen,
          sep="\n"
       )
-      print("—"*64, end = '')
+      print("—"*72, end = '')
       return ""
+   
    def push(self, lan : str): #lan here is long algebraic notation
       start_coor, end_coor = hp.convert_lan(lan)
       
       #check legality
       legal_moves = moves.get_moves(start_coor,self.board)
       
+      # If legal, make move and update data
       if end_coor in legal_moves:
          self.board.array[end_coor] = self.board.array[start_coor]
          self.board.array[start_coor] = 0
+
+         # Update active_color and move count
+         if self.active_color == 'b': 
+            self.active_color = 'w'
+            self.full_move += 1
+            self.half_move += 1
+         else: self.active_color = 'b'
+
       return self
 
 
